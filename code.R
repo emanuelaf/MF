9f485fa00155fed11c834f7bd346303639eb4520
emanuelaf <furfaro.emanuela@gmail.com>
2018-07-09 17:35:41 +0200

    ADD indentation

--- code.R
+++ code.R
@@ -44,45 +44,45 @@ length_s_B_excluded <- numeric(n.sim)
 
 for (i in 1:n.sim)
 {
-s_scr_reduced_size <- sample_scr_reduced_size(data, 
+  s_scr_reduced_size <- sample_scr_reduced_size(data, 
                                               n_A = 100, 
                                               n_B = 200,
                                               n_C = 300)
+  
+  s_scr_new_size_1 <- sample_scr_new_size_1(data, 
+                                            s_scr_reduced_size, 
+                                            n_A = 100, 
+                                            n_B = 200,
+                                            n_C = 300)
 
-s_scr_new_size_1 <- sample_scr_new_size_1(data, 
-                                          s_scr_reduced_size, 
-                                          n_A = 100, 
-                                          n_B = 200,
-                                          n_C = 300)
+  s_scr_new_size_2 <- sample_scr_new_size_2(data,
+                                            n_A = 100, 
+                                            n_B = 200,
+                                            n_C = 300)
 
-s_scr_new_size_2 <- sample_scr_new_size_2(data,
-                                          n_A = 100, 
-                                          n_B = 200,
-                                          n_C = 300)
-
-s_mf <- s_scr_reduced_size
+  s_mf <- s_scr_reduced_size
 
 # number of rejections
-length_s_A_excluded[i] <- nrow(s_scr_reduced_size$s_A_excluded)
-length_s_B_excluded[i] <- nrow(s_scr_reduced_size$s_B_excluded)
+  length_s_A_excluded[i] <- nrow(s_scr_reduced_size$s_A_excluded)
+  length_s_B_excluded[i] <- nrow(s_scr_reduced_size$s_B_excluded)
 
 # costs
 # ampiezza campionaria di fatto dimezzata con metodo reduced_size
-cost_s_scr_reduced_size[i] <- cost(s_scr_reduced_size)
-cost_s_scr_new_size_1[i] <- cost(s_scr_new_size_1)
-cost_s_scr_new_size_2[i] <- cost(s_scr_new_size_2)
-cost_s_mf[i] <- cost(s_mf)
+  cost_s_scr_reduced_size[i] <- cost(s_scr_reduced_size)
+  cost_s_scr_new_size_1[i] <- cost(s_scr_new_size_1)
+  cost_s_scr_new_size_2[i] <- cost(s_scr_new_size_2)
+  cost_s_mf[i] <- cost(s_mf)
 
 # estimators
-s_mf$s_A_final <- s_scr_reduced_size$s_A_initial
-s_mf$s_B_final <- s_scr_reduced_size$s_B_initial
-s_mf$s_C_final <- s_scr_reduced_size$s_C_initial
+  s_mf$s_A_final <- s_scr_reduced_size$s_A_init
+  s_mf$s_B_final <- s_scr_reduced_size$s_B_init
+  s_mf$s_C_final <- s_scr_reduced_size$s_C_init
 
-res_mf_m[i] <- est_mf_multiplicity(s_mf, N_A = N_A, N_B = N_B, N_C=N_C)
-res_mf_ka[i] <- est_mf_ka(s_mf, N_A = N_A, N_B = N_B, N_C = N_C)
-res_strat_reduced[i] <- est_strat(s_scr_reduced_size, N_a = N_a, N_b_ab = N_b_ab, N_C = N_C)
-res_strat_new_size_1[i] <- est_strat(s_scr_new_size_1, N_a = N_a, N_b_ab = N_b_ab, N_C = N_C)
-res_strat_new_size_2[i] <- est_strat(s_scr_new_size_2, N_a = N_a, N_b_ab = N_b_ab, N_C = N_C)
+  res_mf_m[i] <- est_mf_multiplicity(s_mf, N_A = N_A, N_B = N_B, N_C=N_C)
+  res_mf_ka[i] <- est_mf_ka(s_mf, N_A = N_A, N_B = N_B, N_C = N_C)
+  res_strat_reduced[i] <- est_strat(s_scr_reduced_size, N_a = N_a, N_b_ab = N_b_ab, N_C = N_C)
+  res_strat_new_size_1[i] <- est_strat(s_scr_new_size_1, N_a = N_a, N_b_ab = N_b_ab, N_C = N_C)
+  res_strat_new_size_2[i] <- est_strat(s_scr_new_size_2, N_a = N_a, N_b_ab = N_b_ab, N_C = N_C)
 }
 
 # risultati in boxplot
