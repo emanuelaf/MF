7c408872614daf365bb0b0b39c7ad97b220e6591
emanuelaf <furfaro.emanuela@gmail.com>
2018-07-12 15:37:51 +0200

    ADD n_size function for choosing n_q

--- code.R
+++ code.R
@@ -44,23 +44,31 @@ length_s_B_excluded <- numeric(n.sim)
 
 for (i in 1:n.sim)
 {
+# sample size allocation
+  sigma2_alpha <- sigma_alpha_values(data = data, N_A = N_A, N_B = N_B, N_C = N_C)
+  n <- n_size(type = "optimal", N = N, N_A = N_A, N_B = N_B, N_C = N_C, C = 1000, 
+              c_0 = 100, sigma2_alpha = sigma2_alpha, n = 900)
+
   s_scr_reduced_size <- sample_scr_reduced_size(data, 
-                                              n_A = 100, 
-                                              n_B = 200,
-                                              n_C = 300)
+                                              n_A = n$n_A, 
+                                              n_B = n$n_B,
+                                              n_C = n$n_C)
   
   s_scr_new_size_1 <- sample_scr_new_size_1(data, 
                                             s_scr_reduced_size, 
-                                            n_A = 100, 
-                                            n_B = 200,
-                                            n_C = 300)
-
+                                            n_A = n$n_A, 
+                                            n_B = n$n_B,
+                                            n_C = n$n_C)
+  
   s_scr_new_size_2 <- sample_scr_new_size_2(data,
-                                            n_A = 100, 
-                                            n_B = 200,
-                                            n_C = 300)
-
+                                            n_A = n$n_A, 
+                                            n_B = n$n_B,
+                                            n_C = n$n_C)
+  
   s_mf <- s_scr_reduced_size
+  s_mf$s_A_final <- s_scr_reduced_size$s_A_init
+  s_mf$s_B_final <- s_scr_reduced_size$s_B_init
+  s_mf$s_C_final <- s_scr_reduced_size$s_C_init
 
 # number of rejections
   length_s_A_excluded[i] <- nrow(s_scr_reduced_size$s_A_excluded)
@@ -74,10 +82,6 @@ for (i in 1:n.sim)
   cost_s_mf[i] <- cost(s_mf)
 
 # estimators
-  s_mf$s_A_final <- s_scr_reduced_size$s_A_init
-  s_mf$s_B_final <- s_scr_reduced_size$s_B_init
-  s_mf$s_C_final <- s_scr_reduced_size$s_C_init
-
   res_mf_m[i] <- est_mf_multiplicity(s_mf, N_A = N_A, N_B = N_B, N_C=N_C)
   res_mf_ka[i] <- est_mf_ka(s_mf, N_A = N_A, N_B = N_B, N_C = N_C)
   res_strat_reduced[i] <- est_strat(s_scr_reduced_size, N_a = N_a, N_b_ab = N_b_ab, N_C = N_C)
